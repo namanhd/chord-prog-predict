f4cb460d26c5ee15e49876049268b3d9ef8608f6
DoctorN0gloff <nosafegloff@gmail.com>
2019-11-03 00:21:56 -0500

    attempt-1: updated npygenerate to work with new prep funcs

--- attempt-1/npygenerate.py
+++ attempt-1/npygenerate.py
@@ -2,26 +2,24 @@ import prepfuncs
 import os
 import numpy as np
 
-SUBSEQLEN = 8
+SUBSEQLEN = 16
+MIDIDIR = "midifiles/bossa"
 
-for root, dirs, files in os.walk("midifiles"):
+for root, dirs, files in os.walk(MIDIDIR):
     fileslist = list(files)
 
-master_melody_array = []
-master_chords_array = []
-master_output_array = []
+master_melody_array = np.empty((0 ,SUBSEQLEN,12))
+master_chords_array = np.empty((0 ,SUBSEQLEN,12))
+master_output_array = np.empty((0 ,12))
 
 for file in fileslist:
-    mel_subseqs, cho_subseqs_trunc, cho_popped, scorekey = prepfuncs.getArraysFromMidi("midifiles/"+file, SUBSEQLEN, True)
-    master_melody_array += mel_subseqs
-    master_chords_array += cho_subseqs_trunc
-    master_output_array += cho_popped
+    mel_subseqs, cho_subseqs_trunc, cho_popped = prepfuncs.getArraysFromMidi(MIDIDIR + "/" + file, SUBSEQLEN, True)
+    master_melody_array = np.concatenate((master_melody_array, mel_subseqs))
+    master_chords_array = np.concatenate((master_chords_array, cho_subseqs_trunc))
+    master_output_array = np.concatenate((master_output_array, cho_popped))
 
-master_melody_array_np = np.array(master_melody_array)
-master_chords_array_np = np.array(master_chords_array)
-print(master_chords_array_np.shape)
-master_output_array_np = np.array(master_output_array)
+print(master_chords_array.shape)
 
-np.save('input_melody.npy', master_melody_array_np)
-np.save('input_chords.npy', master_chords_array_np)
-np.save('output_chords.npy', master_output_array_np)
+np.save('input_melody.npy', master_melody_array)
+np.save('input_chords.npy', master_chords_array)
+np.save('output_chords.npy', master_output_array)
